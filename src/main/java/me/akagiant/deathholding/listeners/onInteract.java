7d8c3d3b827e42c585bae63b01d2016318bd4bd5
AkaGiant <akagiant01@gmail.com>
2022-04-07 16:06:48 +0100

    v0.3.1
    + CooldownManager.java

--- src/main/java/me/akagiant/deathholding/listeners/onInteract.java
+++ src/main/java/me/akagiant/deathholding/listeners/onInteract.java
@@ -1,6 +1,8 @@
 package me.akagiant.deathholding.listeners;
 
+import me.akagiant.deathholding.managers.CooldownManager;
 import me.akagiant.deathholding.managers.DeathManager;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -11,15 +13,25 @@ import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import java.util.concurrent.TimeUnit;
+
 public class onInteract implements Listener {
+
+    private final CooldownManager cooldownManager = new CooldownManager();
 
     @EventHandler
     public void onInteract(PlayerInteractEntityEvent e) {
         if (e.getRightClicked() instanceof Player) {
             if (e.getPlayer().getInventory().getItemInMainHand().getType() == Material.GOLDEN_APPLE) {
                 Player target = (Player) e.getRightClicked();
-                if (DeathManager.dyingPlayers.contains(target)) {
-                    DeathManager.revivePlayer(target, e.getPlayer());
+                if (DeathManager.dyingPlayers.contains(target.getUniqueId())) {
+                    long timeLeft = System.currentTimeMillis() - cooldownManager.getCooldwon(e.getPlayer().getUniqueId());
+                    if (TimeUnit.MILLISECONDS.toSeconds(timeLeft) >= CooldownManager.COOLDOWN) {
+                        DeathManager.revivePlayer(target, e.getPlayer());
+                        cooldownManager.setCooldown(e.getPlayer(), System.currentTimeMillis());
+                    } else {
+                        e.getPlayer().sendMessage(ChatColor.RED.toString() + (CooldownManager.COOLDOWN - TimeUnit.MILLISECONDS.toSeconds(timeLeft)) + " seconds before you can use this feature again.");
+                    }
                 }
             }
         }
---
+++ src/main/java/me/akagiant/deathholding/managers/CooldownManager.java
@@ -0,0 +1,26 @@
+package me.akagiant.deathholding.managers;
+
+import me.akagiant.deathholding.Main;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class CooldownManager {
+
+    private final HashMap<UUID, Long> cooldowns = new HashMap<>();
+    public static final Long COOLDOWN = Main.config.getConfig().getLong("Revival.Cooldown");
+
+    public void setCooldown (Player player, Long time) {
+        if (player.hasPermission("DeathHolding.Cooldown.Bypass")) return;
+
+        if (time < 1) cooldowns.remove(player.getUniqueId());
+        else cooldowns.put(player.getUniqueId(), time);
+    }
+
+    public Long getCooldwon(UUID player) {
+        return cooldowns.getOrDefault(player, COOLDOWN);
+    }
+
+
+}
